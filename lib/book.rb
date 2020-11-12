b13dcfcbf083339c1a1017525baa781629fe7814
Ian Cossentino <icossentino@gmail.com>
2020-11-12 10:40:45 -0500

    commit tree 6c06f56d12e05a5053c0fd116d37965afc1f7836

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
 
+
+class Book
+
+    attr_accessor :author, :genre, :page_count
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+        @page_count = 0
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        @page_count += 1
+    end
+
+end
+
+
